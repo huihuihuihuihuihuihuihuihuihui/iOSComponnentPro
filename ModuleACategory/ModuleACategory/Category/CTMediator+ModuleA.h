cad9014c20dccf3f095e3c8a3a56f9df1998a753
touna <617973306@qq.com>
2018-10-25 14:44:53 +0800

    1

---
+++ ModuleACategory/ModuleACategory/Category/CTMediator+ModuleA.h
@@ -0,0 +1,17 @@
+//
+//  CTMediator+ModuleA.h
+//  ModuleACategory
+//
+//  Created by touna on 2018/10/25.
+//  Copyright © 2018 ARM. All rights reserved.
+//
+
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (ModuleA)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ModuleACategory/ModuleACategory/Category/CTMediator+ModuleA.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+ModuleA.m
+//  ModuleACategory
+//
+//  Created by touna on 2018/10/25.
+//  Copyright © 2018 ARM. All rights reserved.
+//
+
+#import "CTMediator+ModuleA.h"
+
+@implementation CTMediator (ModuleA)
+
+@end
