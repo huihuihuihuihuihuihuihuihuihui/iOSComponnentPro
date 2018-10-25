b732ce0f4b920072bd0171afc370c039b43d476e
touna <617973306@qq.com>
2018-10-25 14:09:20 +0800

    1

---
+++ iOSComponentDemo/iOSComponentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSComponentDemo
+//
+//  Created by touna on 2018/10/25.
+//  Copyright © 2018 ARM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
